c6e87720a373a604140dbacb6fb9d6fa889b8c60
Xueqian Li <XLi112@slb.com>
2023-11-01 17:03:08 +0800

    feat: implement this test includes function in guess method

--- BullsAndCows/BullsAndCowsGame.cs
+++ BullsAndCows/BullsAndCowsGame.cs
@@ -5,16 +5,23 @@ namespace BullsAndCows
     public class BullsAndCowsGame
     {
         private readonly SecretGenerator secretGenerator;
+        private readonly string secret;
         public BullsAndCowsGame(SecretGenerator secretGenerator)
         {
             this.secretGenerator = secretGenerator;
+            this.secret = secretGenerator.GenerateSecret();
         }
 
         public bool CanContinue => true;
 
         public string Guess(string guess)
         {
-            throw new NotImplementedException();
+            if (this.secret == guess)
+            {
+                return "4A0B";
+            }
+
+            return string.Empty;
         }
     }
 }
