167b5c4b2c513289144eb77d3885b2deed2ff373
Yaxin Yang <yyang66@slb.com>
2023-11-01 20:46:18 +0800

    test: an test that return 2A2B had passed

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -89,5 +89,22 @@ namespace BullsAndCowsTest
             //then
             Assert.Equal("0A2B", result);
         }
+
+        [Theory]
+        [InlineData("1243")]
+        [InlineData("2134")]
+        public void Should_return_2A2B_when_guess_given_position_are_partial_incorrect_but_digit_are_correct(string guessNumber)
+        {
+            //given
+            string secret = "1234";
+            //mock SecretGenerator
+            Mock<SecretGenerator> mockSecretGenerator = new Mock<SecretGenerator>();
+            mockSecretGenerator.Setup(generator => generator.GenerateSecret()).Returns(secret);
+            //when
+            var game = new BullsAndCowsGame(mockSecretGenerator.Object);
+            string result = game.Guess(guessNumber);
+            //then
+            Assert.Equal("2A2B", result);
+        }
     }
 }
