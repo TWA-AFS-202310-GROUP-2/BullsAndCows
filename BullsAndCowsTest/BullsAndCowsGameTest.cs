af706edc05d6e2c2803361fb9651156956153d11
Xueqian Li <XLi112@slb.com>
2023-11-01 17:02:22 +0800

    test: Should_return_4A0B_when_guess_number_and_secret_are_same

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -1,4 +1,5 @@
 using BullsAndCows;
+using Moq;
 using Xunit;
 
 namespace BullsAndCowsTest
@@ -12,6 +13,23 @@ namespace BullsAndCowsTest
             var game = new BullsAndCowsGame(secretGenerator);
             Assert.NotNull(game);
             Assert.True(game.CanContinue);
+        }
+
+        [Fact]
+        public void Should_return_4A0B_when_guess_number_and_secret_are_same()
+        {
+            //given
+            string guessNumber = "1234";
+
+            //when
+            var mockSecretGenerator = new Mock<SecretGenerator>();
+            mockSecretGenerator.Setup(ms => ms.GenerateSecret()).Returns("1234");
+            var game = new BullsAndCowsGame(mockSecretGenerator.Object);
+
+            var res = game.Guess(guessNumber);
+
+            //then
+            Assert.Equal("4A0B", res);
         }
     }
 }
