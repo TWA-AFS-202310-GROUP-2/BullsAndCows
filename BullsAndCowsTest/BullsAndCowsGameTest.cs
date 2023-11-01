fbcb94a3c7fa07227519eb36d2198c75badb0ce1
Tju-Bibibo <34788685+Tju-Bibibo@users.noreply.github.com>
2023-11-01 17:52:20 +0800

    test: add invalid input check test

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -13,5 +13,17 @@ namespace BullsAndCowsTest
             Assert.NotNull(game);
             Assert.True(game.CanContinue);
         }
+
+        [Theory]
+        [InlineData("1123", "Wrong Input, input again")] // 重复数字
+        [InlineData("abcd", "Wrong Input, input again")] // 非数字
+        [InlineData("123", "Wrong Input, input again")] // 少于4位
+        public void Should_return_error_message_for_invalid_input(string input, string expected)
+        {
+            var secretGenerator = new SecretGenerator();
+            var game = new BullsAndCowsGame(secretGenerator);
+            var result = game.Guess(input);
+            Assert.Equal(expected, result);
+        }
     }
 }
